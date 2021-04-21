891380750c4e3c83aaa41c2fa2cb1ae26393ebd0
alexhstrickland <alexhstrickland@gmail.com>
2021-04-21 19:56:13 -0400

    constructing table

--- src/components/results.js
+++ src/components/results.js
@@ -2,7 +2,34 @@ import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Results(props) {
-    return ()
+    return (
+        <div className="container">
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>Photo</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Email</th>
+                        <th>Phone</th>
+                        <th>City</th>
+                        <th>State</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th><img></img></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    )
 }
 
 export default Results;
