08d70f9a7798d5428c34e4b61e9a2957fa601e02
alexhstrickland <alexhstrickland@gmail.com>
2021-04-21 20:59:25 -0400

    sorting issues

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import React from "react";
 import Gallery from "./pages/Gallery";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import Wrapper from "./components/Wrapper";
 
 function App() {
   return (
     <div>
-      <Navbar />
-      <Wrapper>
-        <Gallery />
-      </Wrapper>
-      <Footer />
+      <Gallery />
     </div>
   );
 }
--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,71 +1,70 @@
-import React, { useEffect, useState } from "react";
+import React, { Component } from "react";
 import API from "../utils/API";
-import UserContext from "../utils/userContext";
-import CardContainer from "../components/CardContainer";
-import Row from "../components/Row";
+import Results from "../components/results";
+import Jumbotron from "../components/jumbotron";
+import search from "../components/search";
+import axios from "axios";
 
-function Gallery() {
-  const [users, setUsers] = useState([]);
-  const [user, setUser] = useState({});
-  const [userIndex, setUserIndex] = useState(0);
 
-  // When the component mounts, a call will be made to get random users.
-  useEffect(() => {
-    loadUsers();
-  }, []);
+class Gallery extends Component {
+  state = {
+    employees: [], input: "", sorted: "ascending", searchResults: []};
 
-  function loadUsers() {
-    API.getLanguagesList()
-      .then(languages => {
-        API.getUsersByLanguage(languages[0]).then(users => {
-          setUsers(users);
-          setUser(users[0]);
-        });
-      })
-      .catch(err => console.log(err));
+  // When the component mounts, load the next dog to be displayed
+  componentDidMount() {
+    axios.get("https://randomuser.me/api/?results=100").then(data => {
+      console.log(data.data)
+      this.setState({ 
+        employees: data.data.results,
+        empResults: data.data.results
+      });
+    }).catch(err => {
+      console.log(err)
+    })
   }
 
-  function nextUser(userIndex) {
-    // Ensure that the user index stays within our range of users
-    if (userIndex >= users.length) {
-      userIndex = 0;
+  handleEmpSort = event => {
+  
+    this.setState(newState);
+    
+  };
+
+  handleInputChange = event => {
+    const {name, value} = event.target;
+
+    this.setState({
+      [name]: value
     }
-    setUserIndex(userIndex);
-    setUser(users[userIndex]);
-  }
+    );
+    const empResults = this.state.empResults.filter((employee) => {
+      return employee.name.last.toLowerCase().includes(value.toLowerCase());
+    });
+    this.setState({
+      employees: empResults
+    })
+  };
 
-  function previousUser(userIndex) {
-    // Ensure that the user index stays within our range of users
-    if (userIndex < 0) {
-      userIndex = users.length - 1;
-    }
-    setUserIndex(userIndex);
-    setUser(users[userIndex]);
-  }
+  sortFirstName = () = {};
 
-  function handleBtnClick(event) {
-    // Get the title of the clicked button
-    const btnName = event.target.getAttribute("data-value");
-    if (btnName === "next") {
-      const newUserIndex = userIndex + 1;
-      nextUser(newUserIndex);
-    } else {
-      const newUserIndex = userIndex - 1;
-      previousUser(newUserIndex);
-    }
-  }
+  sortLastName = () = {};
 
-  return (
-    <UserContext.Provider value={{ user, users, handleBtnClick }}>
-      <div>
-        <h1 className="text-center">Welcome to LinkedUp</h1>
-        <h3 className="text-center">Click on the arrows to browse users</h3>
-        <Row>
-          <CardContainer />
-        </Row>
-      </div>
-    </UserContext.Provider>
-  );
+  sortEmail = () = {};
+
+  sortPhone = () = {};
+
+  sortCity = () = {};
+
+  sortState = () = {};
+
+
+  render() {
+    return (
+      <Jumbotron />
+      <Search />
+      <Results />
+    );
+  }
 }
+
 
 export default Gallery;
